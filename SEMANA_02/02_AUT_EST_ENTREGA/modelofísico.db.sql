7b4d988db3945f4289d05c7b12945fb4ae062815
Giovana Katsuki Murata <96273468+katsukimurata@users.noreply.github.com>
2023-04-28 13:39:09 -0300

    atualização

---
+++ SEMANA_02/02_AUT_EST_ENTREGA/modelofísico.db.sql
@@ -0,0 +1,52 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "pessoa " (
+	"cpf"	INTEGER,
+	"nome"	TEXT,
+	"cargo"	TEXT,
+	PRIMARY KEY("cpf" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "informações" (
+	"cpf"	INTEGER,
+	"endereço"	TEXT,
+	"telefone"	INTEGER,
+	"email"	TEXT,
+	"ref"	INTEGER
+);
+CREATE TABLE IF NOT EXISTS "personalidade" (
+	"cpf"	INTEGER,
+	"descrição "	TEXT,
+	"ref"	TEXT,
+	PRIMARY KEY("cpf")
+);
+CREATE TABLE IF NOT EXISTS "habilidade" (
+	"cpf"	INTEGER,
+	"descrição"	TEXT,
+	"ref"	BLOB,
+	PRIMARY KEY("cpf")
+);
+CREATE TABLE IF NOT EXISTS "formação" (
+	"cpf"	INTEGER,
+	"diploma"	TEXT,
+	"curso"	NUMERIC,
+	"ano"	NUMERIC,
+	"instituição"	TEXT,
+	"ref"	TEXT,
+	PRIMARY KEY("cpf")
+);
+CREATE TABLE IF NOT EXISTS "realizações" (
+	"cpf"	INTEGER,
+	"data"	NUMERIC,
+	"descrição"	TEXT,
+	""	INTEGER,
+	"ref"	TEXT,
+	PRIMARY KEY("cpf")
+);
+CREATE TABLE IF NOT EXISTS "experiência" (
+	"nome"	INTEGER,
+	"empresa"	TEXT,
+	"ano"	TEXT,
+	"cargo"	TEXT,
+	"ref"	TEXT,
+	PRIMARY KEY("nome")
+);
+COMMIT;
