50097934d5a5d34d35fac403db7ad15022d27d69
Giovana Katsuki Murata <96273468+katsukimurata@users.noreply.github.com>
2023-04-28 10:55:39 -0300

    ponderada

--- SEMANA_01/01_TUTORIAL/app_11.js
+++ SEMANA_01/01_TUTORIAL/app_11.js
@@ -1,17 +1,17 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3011;
+const http = require('http');// servidor http
+const hostname = '127.0.0.1'; // IP
+const port = 3011;// requisição nessa porta
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<!DOCTYPE html> <head> <title>Etapa 0 - INSTALACAO</title></head> \
-              <body>\
-                <div id="main"> \
-                       <h1> Etapa 1 - INSTALACAO - Servidor Node.js </h1> \
-                       <H2> Meu servidor NODE.js funciona!</H2> </div> \
-              </body> \
-           </html>');
+  res.setHeader('Content-Type', 'text/html'); // cabeçalho
+  res.end('<!DOCTYPE html> <head> <title>Etapa 0 - INSTALACAO</title></head> \ 
+    < body >\
+    <div id="main"> \
+      <h1> Etapa 1 - INSTALACAO - Servidor Node.js </h1> \
+      <H2> Meu servidor NODE.js funciona!</H2> </div> \
+              </body > \
+           </html > ');
 });
-server.listen(port, hostname, () => {
+server.listen(port, hostname, () => { // callback que o servidor é executado 
   console.log(`Server running at http://${hostname}:${port}/`);
 });
