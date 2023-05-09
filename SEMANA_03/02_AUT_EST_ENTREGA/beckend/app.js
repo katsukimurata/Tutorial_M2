411c2781e9d4d7df0acecd311013cbb086f32889
Giovana Katsuki Murata <96273468+katsukimurata@users.noreply.github.com>
2023-05-09 12:01:21 -0300

    endpoints

--- SEMANA_03/02_AUT_EST_ENTREGA/beckend/app.js
+++ SEMANA_03/02_AUT_EST_ENTREGA/beckend/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const sqlite3 = require('sqlite3').verbose();
-const modelofísico = 'modelofísico';
+const modelofísico = 'modelofísico.db.sql';
 
 const hostname = '127.0.0.1';
 const port = 3000;
